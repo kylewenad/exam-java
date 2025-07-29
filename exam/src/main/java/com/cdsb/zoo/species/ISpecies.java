885c497d931eaf93748550b4428610458d9e8673
kylewenad <kylewen@hotmail.com>
2025-07-29 10:58:04 +0200

    Update ISpecies

--- exam/src/main/java/com/cdsb/zoo/species/ISpecies.java
+++ exam/src/main/java/com/cdsb/zoo/species/ISpecies.java
@@ -2,12 +2,16 @@ package com.cdsb.zoo.species;
 
 public interface ISpecies {
 
+    String getNameOfTheAnimal();
+
     String getSpecies();
-    
     String getAnimal();
 
     int getSpaceAnimal();
 
     String getVeterinaryCare();
+
+    Boolean isPredator();
+    Boolean isCompatible();
     
 }
