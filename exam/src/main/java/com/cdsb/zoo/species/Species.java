8039b7b4e423e32802713d8216bde099801f661b
kylewenad <kylewen@hotmail.com>
2025-07-29 10:35:07 +0200

    Update Species to implement ISpecies

--- exam/src/main/java/com/cdsb/zoo/species/Species.java
+++ exam/src/main/java/com/cdsb/zoo/species/Species.java
@@ -1,5 +1,29 @@
 package com.cdsb.zoo.species;
 
-public class Species {
+public class Species implements ISpecies{
+
+    @Override
+    public String getSpecies() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSpecies'");
+    }
+
+    @Override
+    public String getAnimal() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAnimal'");
+    }
+
+    @Override
+    public int getSpaceAnimal() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSpaceAnimal'");
+    }
+
+    @Override
+    public String getVeterinaryCare() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVeterinaryCare'");
+    }
 
 }
