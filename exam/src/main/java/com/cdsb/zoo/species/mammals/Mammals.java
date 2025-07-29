cb3957512184c943c85b41eeb27679c202b9f2b5
kylewenad <kylewen@hotmail.com>
2025-07-29 10:59:03 +0200

    Added to mammals methods of implement ISpecies

--- exam/src/main/java/com/cdsb/zoo/species/mammals/Mammals.java
+++ exam/src/main/java/com/cdsb/zoo/species/mammals/Mammals.java
@@ -1,5 +1,48 @@
 package com.cdsb.zoo.species.mammals;
 
-public class Mammals {
+import com.cdsb.zoo.species.ISpecies;
 
+public abstract class Mammals implements ISpecies{
+
+    @Override
+    public String getAnimal() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getNameOfTheAnimal() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getSpaceAnimal() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getSpecies() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getVeterinaryCare() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean isCompatible() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean isPredator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
