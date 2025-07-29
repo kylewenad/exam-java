6a8b78a13b8a5193bbc5f766e38f64c097a4d6b2
kylewenad <kylewen@hotmail.com>
2025-07-29 10:32:12 +0200

    Added methods in ISpecies

--- exam/src/main/java/com/cdsb/zoo/species/ISpecies.java
+++ exam/src/main/java/com/cdsb/zoo/species/ISpecies.java
@@ -2,4 +2,12 @@ package com.cdsb.zoo.species;
 
 public interface ISpecies {
 
+    String getSpecies();
+    
+    String getAnimal();
+
+    int getSpaceAnimal();
+
+    String getVeterinaryCare();
+    
 }
